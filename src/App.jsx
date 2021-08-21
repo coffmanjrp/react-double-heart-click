a3606835fd4bd284db7b4acbe59fd2c1514cf8dd
coffmanjrp <coffmanjrp@gmail.com>
2021-08-21 12:50:23 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { FaHeart } from 'react-icons/fa';
+import Icon from './components/Icon';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h3>
+        Double click on the image to{' '}
+        <Icon icon={<FaHeart />} className="icon" /> it.
+      </h3>
+      <small>
+        You liked it
+        <span> 0 </span>
+        times
+      </small>
+      <div className="loveMe"></div>
     </div>
   );
 }
